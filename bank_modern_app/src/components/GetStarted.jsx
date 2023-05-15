bfea5684993ac39b2dede64191a8e74cab744b61
Thybow <tmilville.pro@gmail.com>
2023-05-15 22:25:47 +0200

    get started

--- bank_modern_app/src/components/GetStarted.jsx
+++ bank_modern_app/src/components/GetStarted.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import styles from "../style";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () => (
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full
+  bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div>
+      
+    </div>
+  </div>
+)
 
 export default GetStarted
