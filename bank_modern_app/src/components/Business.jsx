6974eb61c01a354e68700b422130233f4086f0f7
Thybow <tmilville.pro@gmail.com>
2023-05-20 21:28:36 +0200

    billing section

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 // FEATURE CARD
 const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+  <div className={`flex-1 flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
     </div>
