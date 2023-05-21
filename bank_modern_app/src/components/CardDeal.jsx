374a7158291cf5370318a30f16b32d5d1c078d14
Thybow <tmilville.pro@gmail.com>
2023-05-21 16:38:49 +0200

    button rounded

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" />
+        in a few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus est ullam
+        voluptas aperiam quas. Voluptates voluptas sit dicta praesentium totam?</p>
+        
+        <Button styles="mt-10" />
+    </div>
+  </section>
+)
 
 export default CardDeal
